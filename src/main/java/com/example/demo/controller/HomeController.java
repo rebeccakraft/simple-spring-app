b757859372b3c3606a40b9b0a899d8ba9beeb451
228161 <rebecca.kraft@aa.com>
2022-08-31 14:30:27 -0700

    added to create a blob

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -2,12 +2,35 @@ package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.microsoft.azure.storage.*;
+import com.microsoft.azure.storage.blob.*;
 
 @RestController
 public class HomeController {
-    
+    String storageConnectionString = "DefaultEndpointsProtocol=https;AccountName=beckydeniedbrdblob;AccountKey=tDYlBRRfjmv3hEsav138IfJHeOLDbdskdmB6zcvELjaR8kmqFwbQeaFkkiGl/isWo7SxrXmUFYHL+ASty5UCBw==;EndpointSuffix=core.windows.net";
     @GetMapping("/")
     public String home() {
-        return "Hello this is becky kraft";
+        try
+        {
+            // Retrieve storage account from connection-string.
+            CloudStorageAccount storageAccount = CloudStorageAccount.parse(storageConnectionString);
+
+            // Create the blob client.
+            CloudBlobClient blobClient = storageAccount.createCloudBlobClient();
+
+            // Get a reference to a container.
+            // The container name must be lower case
+            CloudBlobContainer container = blobClient.getContainerReference("mycontainer");
+
+            // Create the container if it does not exist.
+            container.createIfNotExists();
+        }
+        catch (Exception e)
+        {
+            // Output the stack trace.
+            e.printStackTrace();
+        }
+
+        return "Blob was created";
     }
 }
