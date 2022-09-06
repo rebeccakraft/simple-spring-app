68bcf16ce9c10b2ce2f1371de04e54cf8afd047d
228161 <rebecca.kraft@aa.com>
2022-09-06 14:54:42 -0700

    adding more functions

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -1,4 +1,5 @@
 package com.example.demo.controller;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.microsoft.azure.storage.*;
@@ -6,11 +7,14 @@ import com.microsoft.azure.storage.blob.*;
 
 import java.io.*;
 import java.nio.file.Paths;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.Date;
 
 @RestController
 public class HomeController {
     String storageConnectionString = "DefaultEndpointsProtocol=https;AccountName=beckydeniedbrdblob;AccountKey=tDYlBRRfjmv3hEsav138IfJHeOLDbdskdmB6zcvELjaR8kmqFwbQeaFkkiGl/isWo7SxrXmUFYHL+ASty5UCBw==;EndpointSuffix=core.windows.net";
+    private static final int DATA_PURGE_DAYS = 3;
     @GetMapping("/")
     public String home() {
         String results = "blobs have been saved to C://mydownloads test";
@@ -26,12 +30,7 @@ public class HomeController {
             // Loop over blobs within the container and output the URI to each of them.
             String outputDestination = "/tmp/";
             downloadBlobs(container, outputDestination);
-//            for (ListBlobItem blobItem : container.listBlobs()) {
-//                if (blobItem instanceof CloudBlob) {
-//                    CloudBlob blob = (CloudBlob) blobItem;
-//                    results += blob.getProperties().getCreatedTime().toString();
-//                }
-//            }
+
         }
         catch (Exception e)
         {
@@ -42,7 +41,7 @@ public class HomeController {
     }
 
 
-    public void downloadBlobs(CloudBlobContainer container, String outputDestination)  {
+    private void downloadBlobs(CloudBlobContainer container, String outputDestination)  {
         try {
             for (ListBlobItem blobItem : container.listBlobs()) {
                 // If the item is a blob, not a virtual directory.
@@ -62,5 +61,43 @@ public class HomeController {
             System.out.println(ex);
         }
 
+    }
+    public void readLatestObject(CloudBlobContainer container){
+        LocalDate earlier = LocalDate.now().minusDays(DATA_PURGE_DAYS);
+        try {
+            String earliest = "";
+            String newest = "";
+
+            //this for loop gets name of newest and earliest blob
+            for (ListBlobItem blobItem : container.listBlobs()) {
+                // If the item is a blob, not a virtual directory.
+                if (blobItem instanceof CloudBlob) {
+                    // Download the item and save it to a file with the same name.
+                    CloudBlob blob = (CloudBlob) blobItem;
+                    //if blob is earlier than all others, it is earliest
+                    if(blob.getProperties().getLastModified().before(Date.from(LocalDate.now().plusDays(3).atStartOfDay(ZoneId.systemDefault()).toInstant()))) {
+                        earliest = blob.getName();
+                    }
+                    //if blob is newer than other, it is newest
+                    if(blob.getProperties().getLastModified().after(Date.from(LocalDate.now().minusYears(2).atStartOfDay(ZoneId.systemDefault()).toInstant()))) {
+                        newest = blob.getName();
+                    }
+                }
+            }
+            CloudBlockBlob blobToDelete = container.getBlockBlobReference(earliest);
+            CloudBlockBlob blobToDownload = container.getBlockBlobReference(newest);
+
+            if(blobToDelete.getProperties().getLastModified().before(Date.from(earlier.atStartOfDay(ZoneId.systemDefault()).toInstant()))){
+                System.out.println("Removing " + earliest + " which is earlier than "+ earlier);
+                blobToDelete.deleteIfExists();
+            }
+
+            //then check if latest is the same as previous latest
+
+
+
+        }catch (Exception ex ) {
+            System.out.println("Failed to retrieve data file:"+ ex);
+        }
     }
 }
