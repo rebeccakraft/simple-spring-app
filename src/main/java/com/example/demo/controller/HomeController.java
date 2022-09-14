a718606de755c096a1c49236238af6ce878420ca
rebecca <44142849+rebeccakraft@users.noreply.github.com>
2022-09-14 10:38:40 -0700

    Update HomeController.java

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -13,7 +13,7 @@ import java.util.Date;
 
 @RestController
 public class HomeController {
-    String storageConnectionString = "${{secrets.AZUREKEY}}";
+    String storageConnectionString = ${{secrets.AZUREKEY}};
     private static final int DATA_PURGE_DAYS = 3;
     String results = " ";
     private String filenamePrefix = "test";
